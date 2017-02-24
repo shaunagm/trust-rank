7a8c049d58b8c6caf51b3b53c4297f8aabf53508
shaunagm <shaunagm@gmail.com>
2017-02-24 11:16:55 -0500

    Fix staticfiles settings

--- src/trustrank/settings.py
+++ src/trustrank/settings.py
@@ -129,10 +129,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = '/'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
